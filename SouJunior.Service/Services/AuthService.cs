96e47136832212579810a2ce1b6395249ab5f923
Gabriel Prado Silva <gab.silva@reply.com>
2021-11-28 13:34:04 -0300

    role autentication

--- SouJunior.Service/Services/AuthService.cs
+++ SouJunior.Service/Services/AuthService.cs
@@ -15,12 +15,22 @@ namespace SouJunior.Service.Services
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(AuthenticationHelper.Secret);
+            var role = string.Empty;
+
+            if (user.Empreendedor != null)
+                role = "empreendedor";
+            if (user.EmpresaJr != null)
+                role = "empresajr";
+            if (user.Estudante != null)
+                role = "estudante";
+
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new Claim(ClaimTypes.Name, user.Nome.ToString()),
                     new Claim(ClaimTypes.Email, user.Email.ToString()),
+                    new Claim(ClaimTypes.Role, role),
                 }),
                 Expires = DateTime.UtcNow.AddHours(24),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
